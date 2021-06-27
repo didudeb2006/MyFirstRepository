b77267dbe9bb4e283ce89515829afe571f132d01
mrdeb <mrdeb@LAPTOP-5IP79IG9>
2021-06-27 21:17:07 +0530

    =>2nd commit by dev

--- testproject_tl/src/pack/test/ClassDev.java
+++ testproject_tl/src/pack/test/ClassDev.java
@@ -3,7 +3,7 @@ package pack.test;
 public class ClassDev {
 	
 	public static void main(String args[]) {
-		System.out.println("Inside ClassDev");
+		System.out.println("Inside ClassDev123");
 	}
 
 }
